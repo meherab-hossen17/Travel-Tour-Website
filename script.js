64b8082dac49a08697fb4746f9abfd5a554b0f27
unknown <you@example.com>
2024-05-25 21:23:59 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+     header.classList.toggle("sticky", window.scrollY > 60)
+});
+
+Let menu = document.querySelector('#menu-icon');
+Let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+     menu.classList.toggle('bx-x');
+     navbar.classList.toggle('open');
+}
